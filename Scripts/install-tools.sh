5c63707c90a72cfe0023b65209b88ee2c24a1856
Pavel Serdziukou <iamshvul@gmail.com>
2025-05-10 00:39:50 +0300

    Downgrade xcodegen

--- Scripts/install-tools.sh
+++ Scripts/install-tools.sh
@@ -1,4 +1,4 @@
 export MINT_LINK_PATH=~/bin
-yes  | mint install yonaskolb/xcodegen@2.43.0 --link
+yes  | mint install yonaskolb/xcodegen@2.42.0 --link
 yes  | mint install SwiftGen/SwiftGen@6.6.3 --link
 yes  | mint install nicklockwood/SwiftFormat@0.55.6 --link
